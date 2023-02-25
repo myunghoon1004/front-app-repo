bc120e415ab172318cc173a5acb7ba1bbd973b75
Hooni-Tech <myunghooni.se@gmail.com>
2023-02-25 17:08:04 +0000

    first commit

---
+++ build/precache-manifest.d9cd7a0a308e4eb844644a0816b9b7ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1ce0286b0b63baf56ece63d3d6291e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa34932cf5c9fd436432",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "879c1f8800d3fd79cc31",
+    "url": "/static/js/2.011518c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.011518c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa34932cf5c9fd436432",
+    "url": "/static/js/main.935f72ea.chunk.js"
+  },
+  {
+    "revision": "21f27347af35e687715a",
+    "url": "/static/js/runtime-main.d5ab784a.js"
+  }
+]);
